4eef6dc5bd2422db2e710be8e5eab9b960e15367
biscuitrescue <karttikeyasinha11@gmail.com>
2025-08-24 15:59:12 +0530

    hojicha

--- init.lua
+++ init.lua
@@ -123,5 +123,5 @@ require("cafo.remap")
 require("cafo.lazy")
 require("colorizer").setup()
 
-vim.cmd.colorscheme("roseprime")
+vim.cmd.colorscheme("hojicha")
 -- vim.cmd('hi Normal guibg=NONE ctermbg=NONE')
