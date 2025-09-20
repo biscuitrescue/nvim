40b4cd02660cf80914cd9feccb79b2e94e505d12
biscuitrescue <karttikeyasinha11@gmail.com>
2025-09-21 00:28:24 +0530

    rust lsp maps

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,5 +1,7 @@
 local bufnr = vim.api.nvim_get_current_buf()
 vim.keymap.set("n", "<leader>a", function()
-	vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
+	vim.lsp.buf.code_action() -- supports rust-analyzer's grouping
 	-- or vim.lsp.buf.codeAction() if you don't want grouping.
 end, { silent = true, buffer = bufnr })
+vim.keymap.set("n", "<leader>b", ":Cargo build<CR>")
+vim.keymap.set("n", "<leader>c", ":Cargo check<CR>")
