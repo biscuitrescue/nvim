9f50184ecd6aa6c7bccb134f9103656360bdba44
biscuitrescue <karttikeyasinha11@gmail.com>
2026-03-11 22:09:49 +0530

    -status

--- lua/cafo/lazy.lua
+++ lua/cafo/lazy.lua
@@ -98,18 +98,18 @@ require("lazy").setup({
 	},
 
 	-- UI
-	{
-		"beauwilliams/statusline.lua",
-		dependencies = { "nvim-lua/lsp-status.nvim" },
-		config = function()
-			require("statusline").setup({
-				match_colorscheme = true, -- Enable colorscheme inheritance (Default: false)
-				tabline = true, -- Enable the tabline (Default: true)
-				lsp_diagnostics = true, -- Enable Native LSP diagnostics (Default: true)
-				ale_diagnostics = false, -- Enable ALE diagnostics (Default: false)
-			})
-		end,
-	},
+	-- {
+	-- 	"beauwilliams/statusline.lua",
+	-- 	dependencies = { "nvim-lua/lsp-status.nvim" },
+	-- 	config = function()
+	-- 		require("statusline").setup({
+	-- 			match_colorscheme = true, -- Enable colorscheme inheritance (Default: false)
+	-- 			tabline = true, -- Enable the tabline (Default: true)
+	-- 			lsp_diagnostics = true, -- Enable Native LSP diagnostics (Default: true)
+	-- 			ale_diagnostics = false, -- Enable ALE diagnostics (Default: false)
+	-- 		})
+	-- 	end,
+	-- },
 	"rebelot/kanagawa.nvim",
 	{
 		"lukas-reineke/indent-blankline.nvim",
